f1cc545e4a4d375f8c174c2c4760e2d139f3e967
jonanv <jonan-vargas23@hotmail.com>
2026-06-10 11:12:45 -0500

    🎨 Implement practice case of Factory Pattern

--- 01_creacionales/02-factory-method.ts
+++ 01_creacionales/02-factory-method.ts
@@ -2,13 +2,30 @@
  * ! Factory Method:
  * El patrón Factory Method permite crear objetos sin especificar
  * la clase exacta del objeto que se creará.
- *
- * En lugar de eso, delegamos la creación de objetos a subclases o métodos
- * que encapsulan esta lógica.
- *
- * * Es útil cuando una clase no puede anticipar la clase
- * * de objetos que debe crear.
- *
- * https://refactoring.guru/es/design-patterns/factory-method
- *
- */
+*
+* En lugar de eso, delegamos la creación de objetos a subclases o métodos
+* que encapsulan esta lógica.
+*
+* * Es útil cuando una clase no puede anticipar la clase
+* * de objetos que debe crear.
+*
+* https://refactoring.guru/es/design-patterns/factory-method
+*
+*/
+import { COLORS } from "../helpers/colors.ts";
+
+interface Hamburger {
+    prepare(): void;
+}
+
+class ChickenBurger implements Hamburger {
+    prepare(): void {
+        console.log('Preparando una hamburguesa de %cpollo', COLORS.yellow);
+    }
+}
+
+class BeefHamburger implements Hamburger {
+    prepare(): void {
+        console.log('Preparando una hamburguesa de %cres', COLORS.brown);
+    }
+}
