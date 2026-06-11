74bee961018890c7c2f7ae175ed847b55aff8ecd
jonanv <jonan-vargas23@hotmail.com>
2026-06-11 10:33:49 -0500

    ✨ Add case practice of Inmutability Pattern

--- 01_creacionales/05-inmutabilidad.ts
+++ 01_creacionales/05-inmutabilidad.ts
@@ -2,7 +2,29 @@
  * ! Inmutabilidad con copia
  * Aunque la inmutabilidad es una buena práctica, no siempre es posible.
  * En estos casos, se puede hacer una copia del objeto y modificar la copia.
- *
- *  * Es útil para mantener un historial de estados en aplicaciones interactivas.
- *
- */
+*
+*  * Es útil para mantener un historial de estados en aplicaciones interactivas.
+*
+*/
+import { COLORS } from "../helpers/colors.ts";
+
+class codeEditorState {
+    readonly content: string;
+    readonly cursorPointer: number;
+    readonly unsavedChanges: boolean;
+
+    constructor(content: string, cursorPointer: number, unsavedChanges: boolean) {
+        this.content = content;
+        this.cursorPointer = cursorPointer;
+        this.unsavedChanges = unsavedChanges;
+    }
+
+    public displayState() {
+        console.log('%cEstado del editor: ', COLORS.green);
+        console.log(`
+            Contenido: ${ this.content }
+            Cursor pos: ${ this.cursorPointer }
+            Unsaved changes: ${ this.unsavedChanges }
+        `);
+    }
+}
