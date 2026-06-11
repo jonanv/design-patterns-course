302ac77ccc437adf0679882420f3e3bfe3e0dcc3
jonanv <jonan-vargas23@hotmail.com>
2026-06-11 15:06:19 -0500

    ✨ Create singleton with method getInstance

--- 01_creacionales/06-singleton.ts
+++ 01_creacionales/06-singleton.ts
@@ -9,3 +9,32 @@
  *
  * https://refactoring.guru/es/design-patterns/singleton
  */
+
+
+class DragonBalls {
+    private static instance: DragonBalls;
+
+    private constructor() {
+        
+    }
+
+    static getInstance(): DragonBalls {
+        if (!DragonBalls.instance) {
+            console.log('No existe y se crea');
+            DragonBalls.instance = new DragonBalls();
+        }
+        console.log('Ya existe');
+        return DragonBalls.instance;
+    }
+}
+
+function main() {
+    const dragonBall1 = DragonBalls.getInstance();
+    console.log(dragonBall1);
+
+    const dragonBall2 = DragonBalls.getInstance();
+    console.log(dragonBall2);
+}
+
+main();
+
