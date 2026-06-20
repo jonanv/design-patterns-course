a670555070e5ad61d7b9b824bac4b9a4e8982872
jonanv <jonan-vargas23@hotmail.com>
2026-06-20 15:14:08 -0500

    🎨 Solved TODO of Factory Function Pattern

--- 01_creacionales/07.2-factory-function.ts
+++ 01_creacionales/07.2-factory-function.ts
@@ -30,10 +30,25 @@ function formatDate(date: Date): string {
 // Función fábrica que crea un manejador de logs
 type LogLevel = 'info' | 'warn' | 'error';
 
-function createLogger(level: LogLevel) {
+function createLogger(level: LogLevel): (level: string) => void {
   // Retorna una función que recibe el "message" como argumento
   // Completar: implementar el logger con formato y color para cada nivel
-  throw new Error('Not implemented');
+  return function(message: string): void {
+
+    const logger = {
+      info: `[INFO:${ formatDate(new Date()) }] %c${ message }`,
+      warn: `[WARNING:${ formatDate(new Date()) }] %c${ message }`,
+      error: `[ERROR:${ formatDate(new Date()) }] %c${ message }`
+    }
+
+    const loggerColor = {
+      info: COLORS.blue,
+      warn: COLORS.yellow,
+      error: COLORS.red,
+    }
+
+    return console.log(logger[level], loggerColor[level]);
+  }
 }
 
 // Ejemplo de uso
