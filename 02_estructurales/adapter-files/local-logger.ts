aad3ab3b05e67b70b8a58769a97d0d96a2145ca0
jonanv <jonan-vargas23@hotmail.com>
2026-06-24 16:25:01 -0500

    🐛 Fix colors with the messages

--- 02_estructurales/adapter-files/local-logger.ts
+++ 02_estructurales/adapter-files/local-logger.ts
@@ -7,14 +7,14 @@ export class LocalLogger {
     }
 
     writeLog(msg: string): void {
-        console.log(`[${ this.file } Log] ${ msg }`, COLORS.blue);
+        console.log(`[${ this.file } Log] %c${ msg }`, COLORS.blue);
     }
     
     writeError(msg: string): void {
-        console.log(`[${ this.file } Error] ${ msg }`, COLORS.red);
+        console.log(`[${ this.file } Error] %c${ msg }`, COLORS.red);
     }
 
     writeWarning(msg: string): void {
-        console.log(`[${ this.file } Warning ] ${ msg }`, COLORS.yellow);
+        console.log(`[${ this.file } Warning ] %c${ msg }`, COLORS.yellow);
     }
 }
