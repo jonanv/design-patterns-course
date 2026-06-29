2474771ca4b793b15871888a85ecdefc13b60502
jonanv <jonan-vargas23@hotmail.com>
2026-06-29 07:08:11 -0500

    ✨ Add logger adapter with Adapter Pattern

--- 02_estructurales/adapter-files/logger-adapter.ts
+++ 02_estructurales/adapter-files/logger-adapter.ts
@@ -1,7 +1,36 @@
 import { Logger } from "@deno-library/logger";
 
-const logger = new Logger();
+interface ILoggerAdapter {
+    file: string;
 
-logger.info('');
-logger.warn('');
-logger.error('');
+    writeLog(msg: string): void;
+    writeError(msg: string): void;
+    writeWarning(msg: string): void;
+}
+
+export class DenoLoggerAdpater implements ILoggerAdapter{
+    public file: string;
+    private logger = new Logger();
+
+    constructor(file: string) {
+        this.file = file;
+    }
+
+    writeLog(msg: string): void {
+        this.logger.info(`[${ this.file } Log] ${ msg }`);
+    }
+
+    writeError(msg: string): void {
+        this.logger.warn(`[${ this.file } Error] ${ msg }`);
+    }
+
+    writeWarning(msg: string): void {
+        this.logger.error(`[${ this.file } Warning ] ${ msg }`);
+    }
+}
+
+// const logger = new Logger();
+
+// logger.info('');
+// logger.warn('');
+// logger.error('');
