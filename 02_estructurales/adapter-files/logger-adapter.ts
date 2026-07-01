ff2c808d6af48e204fdc26432fd7d11d38c30a73
jonanv <jonan-vargas23@hotmail.com>
2026-07-01 15:25:39 -0500

    🐛 Fix error with messages

--- 02_estructurales/adapter-files/logger-adapter.ts
+++ 02_estructurales/adapter-files/logger-adapter.ts
@@ -4,8 +4,8 @@ interface ILoggerAdapter {
     file: string;
 
     writeLog(msg: string): void;
-    writeError(msg: string): void;
     writeWarning(msg: string): void;
+    writeError(msg: string): void;
 }
 
 export class DenoLoggerAdpater implements ILoggerAdapter{
@@ -20,11 +20,11 @@ export class DenoLoggerAdpater implements ILoggerAdapter{
         this.logger.info(`[${ this.file } Log] ${ msg }`);
     }
 
-    writeError(msg: string): void {
+    writeWarning(msg: string): void {
         this.logger.warn(`[${ this.file } Error] ${ msg }`);
     }
 
-    writeWarning(msg: string): void {
+    writeError(msg: string): void {
         this.logger.error(`[${ this.file } Warning ] ${ msg }`);
     }
 }
