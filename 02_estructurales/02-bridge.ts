9f18fc0a31fec29eaa665cd589d7dc0df8e979a3
jonanv <jonan-vargas23@hotmail.com>
2026-07-05 18:58:05 -0500

    ✨ Add file for Bridge Pattern

--- 02_estructurales/02-bridge.ts
+++ 02_estructurales/02-bridge.ts
@@ -9,3 +9,49 @@
  *
  * https://refactoring.guru/es/design-patterns/bridge
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+interface Ability {
+    use(): void;
+}
+
+class SwordAttack implements Ability {
+    use(): void {
+        console.log('Ataca con una %cespada ferozmente', COLORS.blue);
+    }
+}
+
+class MagicSpell implements Ability {
+    use(): void {
+        console.log('Lanza un hechizo %cmágico poderoso', COLORS.green);
+    }
+}
+
+abstract class Character {
+    protected ability: Ability;
+
+    constructor(ability: Ability) {
+        this.ability = ability;
+    }
+
+    public setAbility(ability: Ability): void {
+        this.ability = ability
+    }
+
+    abstract performAbility(): void;
+}
+
+class Warrior extends Character {
+    override performAbility(): void {
+        console.log('\nEl guerrerro esta listo para luchar');
+        this.ability.use();
+    }  
+}
+
+class Mage extends Character {
+    override performAbility(): void {
+        console.log('\nEl mago prepara su magia');
+        this.ability.use();
+    }  
+}
