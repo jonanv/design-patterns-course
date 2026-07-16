800393bb53029ceec368800694cfc9ea20ead0ed
jonanv <jonan-vargas23@hotmail.com>
2026-07-16 15:54:21 -0500

    ✨ Add structure for Facade Pattern

--- 02_estructurales/05-facade.ts
+++ 02_estructurales/05-facade.ts
@@ -11,3 +11,51 @@
  *
  * https://refactoring.guru/es/design-patterns/facade
  */
+
+class Proyector {
+    turnOn(): void {
+        console.log('Encendiendo proyector');
+    }
+
+    turnOff(): void {
+        console.log('Apagando proyector');
+    }
+}
+
+class SoundSystem {
+    on(): void {
+        console.log('Sistema de sonido encendido');
+    }
+
+    off(): void {
+        console.log('Sistema de sonido apagado');
+    }
+}
+
+class VideoPlayer {
+    on(): void {
+        console.log('Video player encendido');
+    }
+
+    play(movie: string): void {
+        console.log(`Reproduciendo movie: ${ movie }`);
+    }
+
+    stop(): void {
+        console.log(`Deteniendo película`);
+    }
+
+    off(): void {
+        console.log(`Video player apagado`);
+    }
+}
+
+class PopcornMaker {
+    popingPopcorn(): void {
+        console.log('Haciendo palomitas');
+    }
+
+    turnOffPopingPorncork(): void {
+        console.log(`Deteniendo palomitas`);
+    }
+}
