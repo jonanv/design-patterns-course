0ccd236e9b7c2227268b399a09103aef135a9752
jonanv <jonan-vargas23@hotmail.com>
2026-07-20 14:06:21 -0500

    ✨ Add structure for Flyweight Pattern

--- 02_estructurales/06-flyweight.ts
+++ 02_estructurales/06-flyweight.ts
@@ -8,3 +8,42 @@
  *
  * https://refactoring.guru/es/design-patterns/flyweight
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+interface Location {
+    display(coordinates: { x: number, y: number }): void;
+}
+
+class LocationIcon implements Location {
+    private type: string; // hospital, escula, parque
+    private iconImage: string;
+
+    constructor(type: string, iconImage: string) {
+        this.type = type;
+        this.iconImage = iconImage;
+    }
+
+    display(coordinates: { x: number; y: number; }): void {
+        console.log(`Coords: ${ this.type } en ${ coordinates.x }, ${ coordinates.y } con icono %c[${ this.iconImage }]`, COLORS.green);
+    }
+}
+
+// Fábrica de Flyweights
+// {
+//     escuela: 'assets/school.png',
+//     hospital: 'assets/hospital.png',
+// }
+
+class LocationFactory {
+    private icons: Record<string, LocationIcon> = {};
+
+    getLocationIcon(type: string): LocationIcon {
+        if (!this.icons[type]) {
+            const iconImage = `imagen_de_${ type.toLocaleLowerCase() }.png`;
+            this.icons[type] = new LocationIcon(type, iconImage);
+        }
+        return this.icons[type];
+    }
+
+}
