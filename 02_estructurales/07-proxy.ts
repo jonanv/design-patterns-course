9cc106bf27428d557b94577001d5f9d5ae8907de
jonanv <jonan-vargas23@hotmail.com>
2026-07-24 08:45:23 -0500

    ✨ Add structure for Proxy Pattern

--- 02_estructurales/07-proxy.ts
+++ 02_estructurales/07-proxy.ts
@@ -10,3 +10,45 @@
  * https://refactoring.guru/es/design-patterns/proxy
  *
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+class Player {
+    public name: string;
+    public level: number;
+
+    constructor(name: string, level: number) {
+        this.name = name;
+        this.level = level;
+    }
+}
+interface Room {
+    enter(player: Player): void;
+}
+
+class SecretRoom implements Room {
+    enter(player: Player): void {
+        console.log(`Bienvenido a la sala secreta ${ player.name }`, COLORS.green);
+        console.log('Un gran enemigo te espera');
+    }
+}
+
+// 3. Clase Proxy - Magic Portal
+class MagicPortal implements Room {
+    private secrectRoom: SecretRoom;
+    private minimunLevel: number = 10;
+
+    constructor(room: Room) {
+        this.secrectRoom = room;
+    }
+
+    enter(player: Player): void {
+        if (player.level >= this.minimunLevel) {
+            this.secrectRoom.enter(player);
+            return;
+        }
+
+        console.log(`%cLo sinto mucho ${ player.name } tu nivel ${ player.level } no es suficiente para ingresar, mínimo nivel ${ this.minimunLevel }`, COLORS.red);
+    }
+
+}
