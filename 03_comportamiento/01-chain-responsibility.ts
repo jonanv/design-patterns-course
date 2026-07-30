f0e0a00e682a681c470041acd1c098b939a0005d
jonanv <jonan-vargas23@hotmail.com>
2026-07-30 14:34:12 -0500

    ✨ Create structure for example for Chain Responability Pattern

--- 03_comportamiento/01-chain-responsibility.ts
+++ 03_comportamiento/01-chain-responsibility.ts
@@ -9,3 +9,67 @@
  *
  * https://refactoring.guru/es/design-patterns/chain-of-responsibility
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+interface Handler {
+    setNext(handler: Handler): Handler;
+    handler(request: string): void; 
+}
+
+abstract class BaseHandler implements Handler {
+    private nextHandler?: Handler;
+
+    setNext(handler: Handler): Handler {
+        this.nextHandler = handler;
+        return handler;
+    }
+    
+    handler(request: string): void {
+        if (this.nextHandler) {
+            this.nextHandler.handler(request);
+        }
+    }
+}
+
+// Soporte básico
+class BasicSupport extends BaseHandler {
+
+    override handler(request: string): void {
+        if (request === 'basico') {
+            console.log('%cSoporte básico: Resolviendo problema básico', COLORS.green);
+            return;
+        }
+        
+        console.log('%cSoporte básico: Pasando el problema a soporte avanzado', COLORS.blue);
+        super.handler(request);
+    }
+}
+
+// Soporte técnico
+class TechnicSupport extends BaseHandler {
+
+    override handler(request: string): void {
+        if (request === 'tecnico') {
+            console.log('%cSoporte técnico: Resolviendo problema técnico', COLORS.green);
+            return;
+        }
+        
+        console.log('%cSoporte técnico: Pasando el problema a soporte avanzado', COLORS.blue);
+        super.handler(request);
+    }
+}
+
+// Soporte avanzado
+class AdvanceSupport extends BaseHandler {
+
+    override handler(request: string): void {
+        if (request === 'avanzado') {
+            console.log('%cSoporte técnico: Resolviendo problema avanzado', COLORS.green);
+            return;
+        }
+        
+        console.log('%cSoporte avanzado: Pasando el problema a soporte avanzado', COLORS.blue);
+        super.handler(request);
+    }
+}
