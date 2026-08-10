6e8ee6ce7805658af30ae4e91a48be0077f34ed1
jonanv <jonan-vargas23@hotmail.com>
2026-08-10 09:44:21 -0500

    🎨 Solved TODO of pattern Command

--- 03_comportamiento/02.2-command.ts
+++ 03_comportamiento/02.2-command.ts
@@ -68,20 +68,41 @@ class TextEditor {
 // 3. Clases de Comandos Concretos
 class CopyCommand implements Command {
   private editor: TextEditor;
-
+  
   // TODO: Inyectar el editor en el constructor y el método execute con la acción respectiva
+  constructor(editor: TextEditor) {
+    this.editor = editor;
+  }
+
+  execute(): void {
+    this.editor.copy();
+  }
 }
 
 class PasteCommand implements Command {
   private editor: TextEditor;
 
   // TODO: Inyectar el editor en el constructor y el método execute con la acción respectiva
+  constructor(editor: TextEditor) {
+    this.editor = editor;
+  }
+
+  execute(): void {
+    this.editor.paste();
+  }
 }
 
 class UndoCommand implements Command {
   private editor: TextEditor;
 
   // TODO: Inyectar el editor en el constructor y el método execute con la acción respectiva
+  constructor(editor: TextEditor) {
+    this.editor = editor;
+  }
+
+  execute(): void {
+    this.editor.undo();
+  }
 }
 
 // 4. Clase Cliente - Toolbar
@@ -91,12 +112,17 @@ class Toolbar {
 
   setCommand(button: string, command: Command): void {
     // TODO: Asignar el comando al botón correspondiente
+    this.commands[button] = command;
   }
 
   clickButton(button: string): void {
     //TODO: Ejecutar el comando correspondiente al botón
-
     // TODO: Manejar el caso en que no haya un comando asignado al botón
+    if (this.commands[button]) {
+            this.commands[button].execute();
+            return;
+        }
+
     console.error(`No hay un comando asignado al botón "${button}"`);
   }
 }
