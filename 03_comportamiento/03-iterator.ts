0fa22f38b44cdc3433eae32aa875b1b686963bc1
jonanv <jonan-vargas23@hotmail.com>
2026-08-12 07:45:10 -0500

    ✨ Add methods for pattern Iterator

--- 03_comportamiento/03-iterator.ts
+++ 03_comportamiento/03-iterator.ts
@@ -8,3 +8,46 @@
  *
  * https://refactoring.guru/es/design-patterns/iterator
  */
+
+interface Iterator<T> {
+    next(): T | null;
+    hasNext(): boolean;
+    current(): T | null;
+}
+
+class Pokemon {
+    public name: string;
+    public type: string;
+
+    constructor(name: string, type: string) {
+        this.name = name;
+        this.type = type;
+    }
+}
+
+class PokemonCollection {
+    private pokemons: Pokemon[] = [];
+
+    addPokemon(pokemon: Pokemon): void {
+        this.pokemons.push(pokemon);
+    }
+
+    getPokemonAt(index: number): Pokemon | null {
+        if (index >= 0 && index < this.pokemons.length) {
+            return this.pokemons[index];
+        }
+        return null;
+    }
+
+    getLength(): number {
+        return this.pokemons.length;
+    }
+
+    createIterator() {
+
+    }
+}
+
+class PokemonIterator implements Iterator<Pokemon> {
+    
+}
