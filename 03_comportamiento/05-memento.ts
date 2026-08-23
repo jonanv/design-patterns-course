e6d1520afd85a8a9a9858ea9251ea83b6d700b4b
jonanv <jonan-vargas23@hotmail.com>
2026-08-23 08:44:13 -0500

    ✨ Add pattern Memento

--- 03_comportamiento/05-memento.ts
+++ 03_comportamiento/05-memento.ts
@@ -1,3 +1,5 @@
+import { COLORS } from "../helpers/colors.ts";
+
 /**
  * !Patrón Memento
  * Permite capturar y externalizar un estado interno de un objeto,
@@ -8,3 +10,85 @@
  *
  * https://refactoring.guru/es/design-patterns/memento
  */
+class GameMemento {
+    private level: number;
+    private health: number;
+    private position: string;
+
+    constructor(level: number, health: number, position: string) {
+        this.level = level;
+        this.health = health;
+        this.position = position;
+    }
+
+    getLevel(): number {
+        return this.level;
+    }
+
+    getHealth(): number {
+        return this.health;
+    }
+
+    getPosition(): string {
+        return this.position;
+    }
+}
+
+class Game {
+    private level: number;
+    private health: number;
+    private position: string;
+
+    constructor(level: number, health: number, position: string) {
+        this.level = level;
+        this.health = health;
+        this.position = position;
+
+        console.log(`
+            Jugando en el nivel: ${ this.level }
+            salud: ${ this.health }
+            posición: ${ this.position }
+        `);
+    }
+
+    save(): GameMemento {
+        return new GameMemento(this.level, this.health, this.position);
+    }
+
+    play(level: number, health: number, position: string): void {
+        this.level = level;
+        this.health = health;
+        this.position = position;
+
+        console.log(`
+            Jugando en el nivel: ${ this.level }
+            salud: ${ this.health }
+            posición: ${ this.position }
+        `);
+    }
+
+    restore(memento: GameMemento): void {
+        this.level = memento.getLevel();
+        this.health = memento.getHealth();
+        this.position = memento.getPosition();
+
+        console.log(`
+            \n%cProgreso restaurado
+            %cRestauración en el nivel: %c${ this.level }
+            salud: ${ this.health }
+            posición: ${ this.position }
+        `, COLORS.yellow, COLORS.blue, COLORS.white);
+    }
+}
+
+class GameHistory {
+    private mementos: GameMemento[] = [];
+
+    push(memento: GameMemento): void {
+        this.mementos.push(memento);
+    }
+
+    pop(): GameMemento | null {
+        return this.mementos.pop() ?? null;
+    }
+}
