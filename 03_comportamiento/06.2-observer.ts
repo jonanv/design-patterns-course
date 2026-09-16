dafbe834bf6e7a730ec3a0258bf0f56a5be509bb
jonanv <jonan-vargas23@hotmail.com>
2026-09-16 11:32:17 -0500

    ✨ Add file about pattern Observer

--- 03_comportamiento/06.2-observer.ts
+++ 03_comportamiento/06.2-observer.ts
@@ -24,12 +24,13 @@ interface Observer {
 // Clase Subject - WeatherStation
 // TODO: Terminal la implementación
 class WeatherStation {
-  // observers = [];
-  // weatherData = 'Soleado';
+  private observers: Observer[] = [];
+  private weatherData: string = 'Soleado';
 
   // Agregar un Observer
   subscribe(observer: Observer): void {
-    // TODO: añadir observer
+    this.observers.push(observer);
+    observer.update(this.weatherData);
 
     console.log(
       '%cNueva aplicación suscrita al sistema meteorológico.',
@@ -39,8 +40,7 @@ class WeatherStation {
 
   // Eliminar un Observer
   unsubscribe(observer: Observer): void {
-    // TODO: eliminar observer
-
+    this.observers = this.observers.filter((sub) => sub !== observer);
     console.log(`%cUna aplicación se ha dado de baja`, COLORS.red);
   }
 
@@ -48,13 +48,15 @@ class WeatherStation {
   setWeather(weatherData: string): void {
     console.log(`\nClima actualizado: %c${weatherData}`, COLORS.blue);
 
-    // TODO: actualizar clima y notificar a todos los Observers con el método notifyObservers
+    this.weatherData = weatherData;
+    this.notifyObservers();
   }
 
   // Notificar a todos los Observers
   private notifyObservers(): void {
-    // TODO: implementar método
-    throw new Error('Method not implemented.');
+    for (const observer of this.observers) {
+      observer.update(this.weatherData);
+    }
   }
 }
 
